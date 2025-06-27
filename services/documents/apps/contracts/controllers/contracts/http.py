7bff5256ac8386e0436aa23f3dc379d2cfcc0fe7
Nikita Filonov <nikita.filonov@dif.tech>
2025-06-27 12:03:48 +0300

    handle error

--- services/documents/apps/contracts/controllers/contracts/http.py
+++ services/documents/apps/contracts/controllers/contracts/http.py
@@ -1,6 +1,6 @@
 import uuid
 
-from fastapi import HTTPException
+from fastapi import HTTPException, status
 from pydantic import HttpUrl
 
 from libs.s3.client import S3File
@@ -31,7 +31,13 @@ async def get_contract(
             status_code=error.status_code
         )
 
-    file = await documents_s3_client.get_contract_file(get_account_response.account.id)
+    try:
+        file = await documents_s3_client.get_contract_file(get_account_response.account.id)
+    except Exception as error:
+        raise HTTPException(
+            detail=f"Get contract: {error}",
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
 
     return GetContractResponseSchema(contract=build_contract_from_file(file))
 
