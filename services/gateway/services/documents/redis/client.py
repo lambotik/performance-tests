06261de8c2217ce4ce6bd3619392f06b7536101f
Nikita Filonov <nikita.filonov@dif.tech>
2025-06-27 11:55:58 +0300

    normalize redis cache

--- services/gateway/services/documents/redis/client.py
+++ services/gateway/services/documents/redis/client.py
@@ -1,4 +1,4 @@
-from typing import TypedDict
+from typing import TypedDict, Any
 
 from config import settings
 from libs.logger import get_logger
@@ -23,6 +23,20 @@ def build_contract_document_key(account_id: str) -> str:
     return f'contract_{account_id}'
 
 
+def normalize_tariff_document(value: Any | None) -> TariffDocumentDict | None:
+    return TariffDocumentDict(
+        url=value['url'],
+        document=value['document'].encode()
+    ) if value else None
+
+
+def normalize_contract_document(value: Any | None) -> ContractDocumentDict | None:
+    return ContractDocumentDict(
+        url=value['url'],
+        document=value['document'].encode()
+    ) if value else None
+
+
 class GatewayDocumentsRedisClient(RedisClient):
     async def set_tariff_document(self, account_id: str, document: str):
         await self.set(
@@ -31,7 +45,8 @@ class GatewayDocumentsRedisClient(RedisClient):
         )
 
     async def get_tariff_document(self, account_id: str) -> TariffDocumentDict | None:
-        return await self.get_json(key=build_tariff_document_key(account_id))
+        result = await self.get_json(key=build_tariff_document_key(account_id))
+        return normalize_tariff_document(result)
 
     async def set_contract_document(self, account_id: str, document: str):
         await self.set(
@@ -40,7 +55,8 @@ class GatewayDocumentsRedisClient(RedisClient):
         )
 
     async def get_contract_document(self, account_id: str) -> ContractDocumentDict | None:
-        return await self.get_json(key=build_contract_document_key(account_id))
+        result = await self.get_json(key=build_contract_document_key(account_id))
+        return normalize_contract_document(result)
 
 
 def get_gateway_documents_redis_client() -> GatewayDocumentsRedisClient:
