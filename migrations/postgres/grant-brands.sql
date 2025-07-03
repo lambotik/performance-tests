4070551043b067276ee2e0ade3a806904b832fd0
Nikita Filonov <nikita.filonov@dif.tech>
2025-07-03 12:04:32 +0300

    migrations

--- migrations/postgres/grant-brands.sql
+++
@@ -1,2 +0,0 @@
-GRANT USAGE ON SCHEMA public TO brands_service_user;
-GRANT CREATE ON SCHEMA public TO brands_service_user;
--- migrations/postgres/init-brands.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE brands_service_db;
-CREATE USER brands_service_user WITH PASSWORD 'brands_service_password';
-GRANT ALL PRIVILEGES ON DATABASE brands_service_db TO brands_service_user;
