c4b2fe6222321cadec2d373e554cf2af7026f632
Nikita Filonov <nikita.filonov@dif.tech>
2025-07-03 20:36:17 +0300

    Update postgres.sh

--- scripts/postgres.sh
+++ scripts/postgres.sh
@@ -8,13 +8,11 @@ done
 
 psql -h postgres -U root -f /docker-entrypoint-initdb.d/init-users.sql
 psql -h postgres -U root -f /docker-entrypoint-initdb.d/init-cards.sql
-psql -h postgres -U root -f /docker-entrypoint-initdb.d/init-brands.sql
 psql -h postgres -U root -f /docker-entrypoint-initdb.d/init-accounts.sql
 psql -h postgres -U root -f /docker-entrypoint-initdb.d/init-operations.sql
 
 psql -h postgres -U root -d users_service_db -f /docker-entrypoint-initdb.d/grant-users.sql
 psql -h postgres -U root -d cards_service_db -f /docker-entrypoint-initdb.d/grant-cards.sql
-psql -h postgres -U root -d brands_service_db -f /docker-entrypoint-initdb.d/grant-brands.sql
 psql -h postgres -U root -d accounts_service_db -f /docker-entrypoint-initdb.d/grant-accounts.sql
 psql -h postgres -U root -d operations_service_db -f /docker-entrypoint-initdb.d/grant-operations.sql
 
